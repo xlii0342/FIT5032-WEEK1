66240ea027ec0a43829d8e90440549a25c951bfe
xlii0342 <xlii0342@student.monash.edu>
2024-08-01 10:24:22 +1000

    Add files via upload

---
+++ MyName.vue
@@ -0,0 +1,18 @@
+<script setup>
+defineProps({
+  name: {
+    type: String,
+    required: true
+  }
+})
+</script>
+
+<template>
+  <div class="greetings">
+    <h1 class="green">{{ name }}</h1>
+    <h3>
+      my github link is 
+      <a href="https://github.com/xlii0342" target="_blank" rel="noopener">Vite</a>.
+    </h3>
+  </div>
+</template>
