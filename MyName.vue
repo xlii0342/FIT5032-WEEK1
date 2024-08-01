56a00964ea6fec9e5ce734c25816361ed5ae09f4
xlii0342 <xlii0342@student.monash.edu>
2024-08-01 10:25:50 +1000

    Update MyName.vue

--- MyName.vue
+++ MyName.vue
@@ -12,7 +12,7 @@ defineProps({
     <h1 class="green">{{ name }}</h1>
     <h3>
       my github link is 
-      <a href="https://github.com/xlii0342" target="_blank" rel="noopener">Vite</a>.
+      <a href="https://github.com/xlii0342/FIT5032-WEEK1.git" target="_blank" rel="noopener">Vite</a>.
     </h3>
   </div>
 </template>
